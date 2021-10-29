58a09e84d254be73a656c557fd6144c5b5eeaa17
Rigo Antonio Galicia Barrera <rigo.galicia@bam.com.gt>
2021-10-29 07:41:01 -0600

    Socket server bands names finish

--- sockets/socket.js
+++ sockets/socket.js
@@ -4,13 +4,24 @@ const { Bands } = require('../models/bands');
 const { Band } = require('../models/band');
 
 const bands = new Bands();
-bands.addBand(new Band('Queen'));
 
 io.on('connection', client => {
   client.emit('activeBands', bands.getBands());
 
   client.on('voteBand', (payload) => {
     bands.voteBand(payload.id);
+
+    io.emit('activeBands', bands.getBands());
+  });
+
+  client.on('addBand', (payload) => {
+    bands.addBand(new Band(payload.name));
+
+    io.emit('activeBands', bands.getBands());
+  });
+
+  client.on('removeBand', (payload) => {
+    bands.removeBand(payload.id);
 
     io.emit('activeBands', bands.getBands());
   });
